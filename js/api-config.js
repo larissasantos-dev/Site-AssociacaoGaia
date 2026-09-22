638b01a04c1484a785f13903c4e3511752d733b5
Gabriel Passarinho <gor440744@gmail.com>
2026-09-22 19:02:05 -0300

    Consertado o kit de acessibilidade e menu do mobile quebrando

---
+++ js/api-config.js
@@ -0,0 +1,3 @@
+// URL pública da API Spring Boot no Render.
+// Ao trocar o serviço/domínio, atualize este valor e publique novamente o front-end.
+window.GAIA_API_BASE = 'https://associacao-gaia-api.onrender.com';
